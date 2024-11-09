20089259a8dae305f4e4561480699fbf285d3ed4
jackma2003 <jiahaoma2000@gmail.com>
2024-11-08 19:32:17 -0500

    bug fix 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import TaskForm from './create_task'
 import './input.css'
 import LoginPage from './login'
 import RegisterPage from './register'
