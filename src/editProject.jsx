3e58a3a2d392331c117e88d71f3075a34d88e147
jackma2003 <jiahaoma2000@gmail.com>
2024-11-12 00:37:49 -0500

    import fix

--- src/editProject.jsx
+++ src/editProject.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { ArrowLeft, Save } from "lucide-react";
+import "./input.css";
 
 const EditProject = () => {
     const { id } = useParams(); 
